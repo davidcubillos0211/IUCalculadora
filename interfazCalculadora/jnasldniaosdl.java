afc33ef1005d21631abf1faf936c2f170ce41124
davidcubillos0211 <100205678+davidcubillos0211@users.noreply.github.com>
2022-03-28 10:14:09 +0200

    Add files via upload

---
+++ interfazCalculadora/jnasldniaosdl.java
@@ -0,0 +1,11 @@
+package interfazCalculadora;
+
+public class jnasldniaosdl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String v= "GCCTTGTGGTACTGCCTGATAGGGTCGTTGCGAGTGCCCCGGGAGGTCTCGTAGACCGTGCACCATGAGCA";
+System.out.print(v.length());
+	}
+
+}
